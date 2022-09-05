24b11a58d5ced38d34acece02dbcb7ac9ce04532
Tish Faroul <lfaroul14@gmail.com>
2022-09-05 17:41:15 -0400

    working on the newTea form

--- sweett-client/src/components/NewTea.js
+++ sweett-client/src/components/NewTea.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 function NewTea() {
+
+    const [form, setForm] = useState({
+        name: '',
+        img: ''
+    })
+
+    function handleChange(e){
+        const { name, value} = e.target;
+
+        setForm(prevForm => {
+            return {
+                ...prevForm, 
+                [name]: value
+            }
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(form);
+    }
+
+
+
   return (
     <div>
         <form>
             <div>
                 <label> Tea name:</label>
-                <input type='text'></input>
+                <input type='text' onChange={handleChange} name='name' value={form.name}></input>
             </div>
 
             <div>
                 <label> Image URL:</label>
-                <input></input>
+                <input type='text' onChange={handleChange} name='img' value={form.img}></input>
             </div>
+
+            <button onClick={handleSubmit}>Submit Tea</button>
         </form>
     </div>
   )

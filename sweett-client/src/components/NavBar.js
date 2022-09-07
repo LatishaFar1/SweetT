69bce15033286a59f955bfeea91693a1916efc7a
Tish Faroul <lfaroul14@gmail.com>
2022-09-06 22:54:36 -0400

    continuing work on the navbar

--- sweett-client/src/components/NavBar.js
+++ sweett-client/src/components/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
-import { flexCenter } from '../theme/styles';
+import { flexCenter, dFlex, flexBetweenCenter } from '../theme/styles';
 import logo from '../photos/logo.jpeg';
+import Container from '@mui/material/Container';
 
 function NavBar() {
   return (
     <div>
-      <Box sx={flexCenter}>
-        <img src={logo}/>
+      <Box sx={{...dFlex, minHeight: 50, borderBottom: '1px solid #ddd'}}>
+        <img src={logo} size={20} />
+       <Container>
+
+       </Container>
       </Box>
     </div>
   )

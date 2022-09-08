db5220cb4f4e260134d58b1428ad502c5eac7cbc
Tish Faroul <lfaroul14@gmail.com>
2022-09-07 21:41:48 -0400

    changing around the navbar

--- sweett-client/src/components/NavBar.js
+++ sweett-client/src/components/NavBar.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Box from '@mui/material/Box';
 import { flexCenter, dFlex, flexBetweenCenter } from '../theme/styles';
 import logo from '../photos/logo.jpeg';
 import Container from '@mui/material/Container';
 import {Link} from "react-router-dom";
+import {AiOutlineHome, AiOutlineCoffee} from 'react-icons/ai';
 
 function NavBar() {
+
+
+
   return (
     <div>
+
+
+      <nav className='nav'>
+            <ul className='links'>
+              <li>
+                
+                <Link to='/'>
+                  <AiOutlineHome size={20}/>
+              </Link>
+
+
+              <Link to='/tea'>
+                <AiOutlineCoffee size={20}/>
+              </Link>
+
+            
+              </li>
+            </ul>
+
+         </nav>
+
       <Box sx={{...dFlex, minHeight: 50, borderBottom: '1px solid #ddd'}}>
     
 
@@ -21,18 +46,23 @@ function NavBar() {
           </Box>
        </Container>
 
-          <div className='nav'>
-
-              <Link to='/'>
-                  Home
-              </Link>
 
 
-              <Link to='/tea'>
-                  Tea
-              </Link>
+          {/* <div className='nav'>
+            <div className='nav-menu'>
+      
+                  <ul className='links'>
 
-          </div>
+                  </ul>
+             
+              </div>
+            </div>
+         */}
+
+
+
+              
+    
 
       </Box>
     </div>
